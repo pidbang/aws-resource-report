6001e81d408cac825206d666e2f539d5df7ec679
Dominik Pawlak <dpawlak@sq3-partners.com>
2024-07-22 18:11:06 +0200

    Add tag searching for AIM resources

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This report checks all available regions. It filters out some resources, that seem to be created in each region by default.
 
+IAM User and Role tags seem not to be indexed in Resource Explorer.
+For each found user / role, they are checked independently in IAM.
+
 The following policy must be in place to run this report:
 
 ```json
@@ -12,8 +15,10 @@ The following policy must be in place to run this report:
             "Sid": "VisualEditor0",
             "Effect": "Allow",
             "Action": [
+                "iam:ListRoleTags",
                 "account:ListRegions",
-                "resource-explorer-2:Search"
+                "resource-explorer-2:Search",
+                "iam:ListUserTags"
             ],
             "Resource": "*"
         }
--- report.py
+++ report.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from botocore.paginate import PageIterator
 
 
 def main():
@@ -8,13 +9,14 @@ def main():
         'MaxItems': 1000,
         'PageSize': 10,
     }
+    iam_client = boto3.client('iam')
     client = boto3.client('sts')
     response = client.get_caller_identity()
     account_id = response['Account']
     client = boto3.client('account')
-    paginator = client.get_paginator('list_regions')
-    regions_iterator = paginator.paginate(
-        RegionOptStatusContains=[ 'ENABLED', 'ENABLING', 'ENABLED_BY_DEFAULT'],
+    search_paginator = client.get_paginator('list_regions')
+    regions_iterator = search_paginator.paginate(
+        RegionOptStatusContains=['ENABLED', 'ENABLING', 'ENABLED_BY_DEFAULT'],
         PaginationConfig=pagination_config
     )
     regions = ["global"]
@@ -22,7 +24,7 @@ def main():
         for region in regions_page["Regions"]:
             regions.append(region['RegionName'])
     client = boto3.client('resource-explorer-2')
-    paginator = client.get_paginator('search')
+    search_paginator = client.get_paginator('search')
     for region in regions:
         print(f"# {region}")
         query_items = [
@@ -39,15 +41,44 @@ def main():
             f"-id:arn:aws:athena:{region}:{account_id}:datacatalog/AwsDataCatalog",
             f"-id:arn:aws:s3:{region}:{account_id}:storage-lens/default-account-dashboard",
             f"-id:arn:aws:iam::{account_id}:mfa/root-account-mfa-device",
+            f"-id:arn:aws:iam::{account_id}:role/aws-service-role/*",
         ]
-        resource_page_iterator = paginator.paginate(
+        resource_page_iterator = search_paginator.paginate(
             QueryString=" ".join(query_items),
             PaginationConfig=pagination_config
         )
         for resource_page in resource_page_iterator:
             for resource in resource_page["Resources"]:
+                if resource['ResourceType'] == "iam:user":
+                    role_arn = resource['Arn']
+                    role_name = role_arn[role_arn.index(":user/")+6:]
+                    role_tags_paginator = iam_client.get_paginator('list_user_tags')
+                    role_tags_page_iterator = role_tags_paginator.paginate(
+                        UserName=role_name,
+                        PaginationConfig=pagination_config
+                    )
+                    if is_tagged(role_tags_page_iterator):
+                        continue
+                elif resource['ResourceType'] == "iam:role":
+                    role_arn = resource['Arn']
+                    role_name = role_arn[role_arn.index(":role/")+6:]
+                    role_tags_paginator = iam_client.get_paginator('list_role_tags')
+                    role_tags_page_iterator = role_tags_paginator.paginate(
+                        RoleName=role_name,
+                        PaginationConfig=pagination_config
+                    )
+                    if is_tagged(role_tags_page_iterator):
+                        continue
                 print(json.dumps(resource, indent="  ", default=str))
     print("done")
+
+
+def is_tagged(tags_page_iterator: PageIterator) -> bool:
+    for tags_page in tags_page_iterator:
+        for tag in tags_page["Tags"]:
+            if tag['Key'] == "resource_group":
+                return True
+    return False
 
 
 if __name__ == "__main__":
